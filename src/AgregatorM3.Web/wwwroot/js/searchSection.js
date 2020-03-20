ca9dcc6d14d946f7d8e05242485ce2cf359d149a
pawel.rzeczkowski <pawel.rzeczkowski@stepstone.com>
2020-03-20 17:29:54 +0000

    feat: added search params to front and implemented new collapsible ajax search form

--- src/AgregatorM3.Web/wwwroot/js/searchSection.js
+++ src/AgregatorM3.Web/wwwroot/js/searchSection.js
@@ -1,11 +1,34 @@
 ﻿$('#search').click(function (event) {
-    getData();
+    //getData();
+    collapseSearch();
     event.preventDefault();
+});
+
+$('.collapsible').click(function () {
+    let collapse = document.getElementsByClassName("collapsible");
+    let content = collapse[0].nextElementSibling;
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
 });
 
 function getData() {
     $.post('/search/GetData', { parameters: {
-        priceMin: $('#priceMin').val(),
-        priceMax: $('#priceMax').val()
+        PriceFrom: $('#PriceFrom').val(),
+        PriceTo: $('#PriceTo').val(),
+        SurfaceFrom: $('#SurfaceFrom').val(),
+        SurfaceTo: $('#SurfaceTo').val(),
+        RoomsFrom: $('#RoomsFrom').val(),
+        RoomsTo: $('#RoomsTo').val(),
+        PricePerMeterTo: $('#PricePerMeterTo').val()
     }});
 }
+
+function collapseSearch() {
+    let collapse = document.getElementsByClassName("collapsible");
+    let content = collapse[0].nextElementSibling;
+    content.style.maxHeight = null;
+}
+
